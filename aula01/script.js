f2ff816746d870e0da925cf12c852eb59e462a1f
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-04-30 21:52:34 -0300

    Add animations

--- aula01/script.js
+++ aula01/script.js
@@ -1,17 +1,33 @@
 var canvas, context
-var HEIGHT, WIDTH
 var circle, square, triangle
+var WIDTH, HEIGHT
 
 var drawCircle = {
     r: 100,
     x: 300,
     y: 300,
+    //dir => direction, 0 = right, 1 = left
+    dir: 0,
     color: '#FFFF00',
     draw: function() {
         context.beginPath()
         context.fillStyle = this.color
         context.arc(this.x, this.y, this.r, 0, (Math.PI * 2))
         context.fill()
+    },
+    move: function(){
+        if((this.x  + 100)  >= WIDTH){
+            this.dir = 1
+        }
+        if((this.x  - 100) <= 0){
+            this.dir = 0
+        }
+        if(this.dir === 0){
+            this.x++
+        }
+        else {
+            this.x--
+        }
     }
 }
 var drawSquare = {
@@ -19,31 +35,84 @@ var drawSquare = {
     h: 200,
     x: 200,
     y: 400,
+    dir: 1,
     color: '#0000FF',
     draw: function(){
         context.fillStyle = this.color
         context.fillRect(this.x, this.y, this.w, this.h)
+    },
+    move: function() {
+        if((this.x + this.w)>= WIDTH){
+            this.dir = 1
+        }
+        if(this.x  === 0){
+            this.dir = 0
+        }
+        if(this.dir === 0){
+            this.x++
+        }
+        else{
+            this.x--
+        }
     }
 }
 var drawTriangle = {
+    x1:300, x2: 200, x3: 400,
+    y1:200, y2: 400, y3: 400,
     color: '#00FF00',
+    dir: 0,
     draw: function() {
         context.fillStyle = this.color
         context.beginPath()
-        context.moveTo(300, 200)
-        context.lineTo(200, 400)
-        context.lineTo(400, 400)
+        context.moveTo(this.x1, this.y1)
+        context.lineTo(this.x2, this.y2)
+        context.lineTo(this.x3, this.y3)
         context.fill()
+    },
+    move: function() {
+        if(this.x3 >= WIDTH){
+            this.dir = 1
+        }
+        if(this.x2 === 0){
+            this.dir = 0
+        }
+        if(this.dir === 0){
+            this.x1 += 2
+            this.x2 += 2
+            this.x3 += 2
+        }
+        else {
+            this.x1 -= 2
+            this.x2 -= 2
+            this.x3 -= 2
+        }
     }
-}
 
-function execute(){
+}
+function draw(){
+    backgroundCanvas()
+
     drawCircle.draw()
     drawSquare.draw()
     drawTriangle.draw()
 }
+function update(){
+    drawCircle.move()
+    drawSquare.move()
+    drawTriangle.move()
+}
+function backgroundCanvas(){
+    context.fillStyle = '#FFF'
+    context.fillRect(0, 0, WIDTH, HEIGHT)
+}
+function execute(){
+    draw()
+    update()
 
-function main(params) {
+    window.requestAnimationFrame(execute)
+}
+
+function main() {
     WIDTH = window.innerHeight
     HEIGHT = window.innerWidth
 
