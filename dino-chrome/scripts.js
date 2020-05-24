16666b486dcd27220d4cd2afaba6acab57517c52
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-05-23 23:42:17 -0300

    Add obstáculos

--- dino-chrome/scripts.js
+++ dino-chrome/scripts.js
@@ -20,9 +20,11 @@ var cactus = {
             this.time--
         
         for(let i = 0; i < this.vector.length; i++){
-            let v = this.vector[i]
-            
-            v.x-=3
+            this.vector[i].x -= 3
+
+            if(this.vector[i].x <= -this.vector[i].w){
+                this.vector.splice(i, 1)
+            }
         }
     },
     draw: function(){
@@ -44,6 +46,8 @@ var cactusBackground = {
         this.vector.push({
             name: './assets/cactus1.gif',
             x: window.innerWidth,
+            w: 50,
+            h: 30
         })
 
         this.time = 400
@@ -55,19 +59,21 @@ var cactusBackground = {
             this.time--
         
         for(let i = 0; i < this.vector.length; i++){
-            let v = this.vector[i]
-            
-            v.x--
+            this.vector[i].x--
+
+            if(this.vector[i].x <= -this.vector[i].w){
+                this.vector.splice(i, 1)
+            }
         }
     },
     draw: function(){
+    
         for(let i = 0; i < this.vector.length; i++){
-            let v = this.vector[i]
-
             let c1 = new Image()
-            c1.src = v.name
+            
+            c1.src = this.vector[i].name
 
-            context.drawImage(c1, v.x, 30, 50, 100)
+            context.drawImage(c1, this.vector[i].x, this.vector[i].h, this.vector[i].w, 100)
         }
     } 
 }
@@ -86,6 +92,7 @@ var ball = {
     x: 20,
     y: 0,
     color: '#FFF000',
+    status: false,        // a bola esta no ar
     draw: function(){
         context.beginPath()
 
@@ -95,13 +102,55 @@ var ball = {
     },
     drop: function(){
         if((this.y + this.r) <= 180)
-            this.y += 2
+            this.y += 4
+        else
+            this.status = true
+    },
+    jump: function() {
+        this.y -= 150
+        this.status = false
+    }
+}
+var barriers = {
+    vet: [],
+    colors: ['#0000ff', '#ff0000', '#00ff00', '#ffff00', '#ff00ff'],
+    time: 0,
+    insert: function(){
+        this.vet.push({
+            x: window.innerWidth,
+            color: this.colors[Math.floor(5 * Math.random())],
+            h: 50 + Math.floor(30 * Math.random()),
+            w: 40 + Math.floor(20 * Math.random())
+        })
+
+        this.time = 100 + Math.floor(50 * Math.random())
+    },
+    update: function(){
+        if(this.time == 0)
+            this.insert()
+        else
+            this.time--
+        
+        for(let i = 0; i < this.vet.length; i++){
+            this.vet[i].x -= 6;
+            
+            if(this.vet[i].x <= -this.vet[i].w){
+                this.vet.splice(i, 1)
+            }
+        }
+    },
+    draw: function(){
+        for(let i = 0; i < this.vet.length; i++){
+            context.fillStyle = this.vet[i].color
+            context.fillRect(this.vet[i].x, (HEIGHT - this.vet[i].h), this.vet[i].w, this.vet[i].h)
+        }
     }
 }
 
 function update(){
     cactusBackground.update()
     cactus.update()
+    barriers.update()
     ball.drop()
 }
 function draw(){
@@ -110,6 +159,7 @@ function draw(){
     floor.draw()
     cactusBackground.draw()
     cactus.draw()
+    barriers.draw()
 
     ball.draw()
 }
@@ -128,6 +178,20 @@ function updateBackgroundCanvas(){
     context.fillStyle = background
     context.fillRect(0, 0, WIDTH, HEIGHT)
 }
+function KeyDown(evt){
+    switch(evt.keyCode){
+        case 65:            // seta <-
+            ball.x -= 10
+        break
+        case 68:            // seta ->
+            ball.x += 10
+        break
+        case 32:
+            if(ball.status)
+                ball.jump()
+        break
+    }
+}
 function main(){
     HEIGHT = 200
     WIDTH = window.innerWidth
@@ -140,6 +204,9 @@ function main(){
     context = canvas.getContext('2d')
 
     document.body.appendChild(canvas)
+
+    window.addEventListener('keydown', KeyDown, true)
+    
 
     execute()
 }
